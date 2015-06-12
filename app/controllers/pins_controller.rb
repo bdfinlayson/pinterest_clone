9da7c455a9e447bf041b959518d9bb64ad400489
Bryan Finlayson <bdfinlayson@gmail.com>
2015-06-12 11:02:22 -0500

    Add pins to index

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,8 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
   def index
+    @pins = Pin.all.order('created_at DESC')
 
   end
 
@@ -19,9 +22,18 @@ class PinsController < ApplicationController
     end
   end
 
+  def show
+
+  end
+
   private
 
   def pin_params
     params.require(:pin).permit(:title, :description)
+  end
+
+  def find_pin
+    # using private method for this so multiple methods can use it
+    @pin = Pin.find(params[:id])
   end
 end
